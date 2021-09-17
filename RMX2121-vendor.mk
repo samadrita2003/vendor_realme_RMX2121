ef0f996b34b7b047b1198e067a345c8c17804f44
Advaith Bhat <advaithbhat9@gmail.com>
2021-09-17 05:44:04 +0000

    RMX2121: Remove apn-configs from vendor tree
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- RMX2121-vendor.mk
+++ RMX2121-vendor.mk
@@ -19,7 +19,6 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_COPY_FILES += \
     vendor/realme/RMX2121/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
-    vendor/realme/RMX2121/proprietary/product/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \\
     vendor/realme/RMX2121/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
     vendor/realme/RMX2121/proprietary/lib/libem_support_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libem_support_jni.so \
     vendor/realme/RMX2121/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
