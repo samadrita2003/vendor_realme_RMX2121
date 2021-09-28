7f3bac32c5b3611326b61be79bb40ae1cb299c12
Advaith Bhat <advaithbhat9@gmail.com>
2021-09-28 04:20:23 +0000

    RMX2121: Update kernel Modules from C04
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- RMX2121-vendor.mk
+++ RMX2121-vendor.mk
@@ -28,11 +28,6 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2121/proprietary/lib64/libem_support_jni.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libem_support_jni.so \
     vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/bt_drv.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/bt_drv.ko \
     vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/conninfra.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/conninfra.ko \
-    vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/fmradio_drv.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/fmradio_drv.ko \
-    vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/fpsgo.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/fpsgo.ko \
-    vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/gps_drv.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/gps_drv.ko \
-    vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/kheaders.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/kheaders.ko \
-    vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/met.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/met.ko \
     vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/wlan_drv_gen4m.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/wlan_drv_gen4m.ko \
     vendor/realme/RMX2121/proprietary/product/vendor_overlay/30/lib/modules/wmt_chrdev_wifi.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/wmt_chrdev_wifi.ko \
 
